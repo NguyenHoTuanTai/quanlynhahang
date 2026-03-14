122c67503b4f347a98b590789e5cd1393758e332
NguyenHoTuanTai <tuantainguyenho@gmail.com>
2026-03-14 22:46:07 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+LOGIN_REDIRECT_URL = '/redirect/'
+LOGOUT_REDIRECT_URL = '/'
