8118f0b3239d5cac645cdc00fa04965b1127d6a8
NguyenHoTuanTai <tuantainguyenho@gmail.com>
2026-03-27 08:40:44 +0700

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -13,5 +13,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('redirect/', views.redirect_after_login, name='redirect'),
+    path('dat-ban/', views.dat_ban_view, name='dat_ban'),
+    path('thanh-toan/<int:dat_ban_id>/', views.thanh_toan_view, name='thanh_toan'),
+    path('nhan-vien/', views.man_hinh_nhan_vien, name='man_hinh_nhan_vien'),
+    path('quan-ly-dat-ban/', views.quan_ly_dat_ban, name='quan_ly_dat_ban'),
+    path('dashboard/', views.man_hinh_nhan_vien, name='dashboard'),
+    path('thuc-don/', views.thuc_don, name='thuc_don'),
+    path('ban/<int:ban_id>/', views.chi_tiet_ban, name='chi_tiet_ban'),
 ]
 
