0cf6ea6de8f679a2d8e1274b549150dd66a0f4ac
NguyenHoTuanTai <tuantainguyenho@gmail.com>
2026-04-17 16:27:03 +0700

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -129,10 +129,3 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/redirect/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'anhtaimonkey222@gmail.com'
-EMAIL_HOST_PASSWORD = 'sond jntr rgza ylsp'
